daf46b20c98200ef8e7da7c448c0ff97e9fc07f9
Francisco Borges Aurindo Barros <francisco.borges.aurindo.barros@cern.ch>
2021-06-15 14:05:39 +0000

    Restore oidc v1

--- images/drupal-operations-scripts/site-install.sh
+++ images/drupal-operations-scripts/site-install.sh
@@ -9,8 +9,9 @@ drush sql:drop -y
 
 # Install Drupal site
 echo "Installing Drupal site"
-drush site-install cern -y --config-dir=../config/sync --account-name=admin
-
+drush site-install cern -y --config-dir=../config/sync --account-name=admin install_configure_form.enable_update_status_emails=NULL
+# Remove admin account
+drush user-cancel admin -y
 drush cr
 if [ "$?" -ne "0" ]; then
     drush cr
