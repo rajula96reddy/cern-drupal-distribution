c70e30940b629220785b6145c2edac5a1a1e8da2
Konstantinos Samaras-Tsakiris <konstantinos.samaras-tsakiris@cern.ch>
2021-09-13 11:00:19 +0200

    Merge branch 'debug-site-install' into 'master'
    Update site-install script with verbose output
    
    See merge request drupal/paas/cern-drupal-distribution!19

--- images/drupal-operations-scripts/ensure-site-install.sh
+++ images/drupal-operations-scripts/ensure-site-install.sh
@@ -18,7 +18,7 @@ drush sql:drop -y
 
 # Install Drupal site
 echo "Installing Drupal site"
-drush site-install cern -y --config-dir=../config/sync --account-name=admin install_configure_form.enable_update_status_emails=NULL
+drush site-install cern -y --config-dir=../config/sync --account-name=admin install_configure_form.enable_update_status_emails=NULL -vvv
 # Remove admin account
 drush user-cancel admin -y
 drush cr
