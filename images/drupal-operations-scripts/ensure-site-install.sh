7a2c0dde5acc318e18db6e11338ce709a80979f7
Vineet Reddy Rajula <rajula.vineet.reddy@cern.ch>
2021-11-03 17:56:19 +0100

    Enable redis module in site install script

--- images/drupal-operations-scripts/ensure-site-install.sh
+++ images/drupal-operations-scripts/ensure-site-install.sh
@@ -19,6 +19,10 @@ drush sql:drop -y
 # Install Drupal site
 echo "Installing Drupal site"
 drush site-install cern -y --config-dir=../config/sync --account-name=admin install_configure_form.enable_update_status_emails=NULL -vvv
+
+# Enable redis
+drush pm:enable redis
+
 # We double check that cache does not interfere with user deletion, details can be seen here: https://gitlab.cern.ch/drupal/paas/cern-drupal-distribution/-/merge_requests/33
 drush cr
 if [ "$?" -ne "0" ]; then
@@ -30,9 +34,6 @@ drush cr
 if [ "$?" -ne "0" ]; then
     drush cr
 fi
-
-# Run enable or disable redis script
-/operations/enable-or-disable-redis.sh
 
 # Leave cookie
 touch $INSTALL_COOKIE
