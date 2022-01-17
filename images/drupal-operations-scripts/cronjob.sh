21d4a08943187204d39a14302dc24bee11c8f897
Konstantinos Samaras-Tsakiris <konstantinos.samaras-tsakiris@cern.ch>
2022-01-17 13:32:27 +0100

    Merge branch 'new-cronjob-script' into 'master'
    New cronjob script
    
    See merge request drupal/paas/cern-drupal-distribution!73

--- images/drupal-operations-scripts/cronjob.sh
+++ images/drupal-operations-scripts/cronjob.sh
@@ -5,7 +5,6 @@
 # Issue link: https://gitlab.cern.ch/webservices/webframeworks-planning/-/issues/437
 # Note: If in the future we want to have more periodic actions made to the Drupalsites
 # or want to trigger CronTasks in a different way, all we need is to edit this script.
-# The DrupalSite operator is only involved to create a CronJob that executes this script.
 
 usage() {
   echo "Usage: $0 --site <site>" 1>&2;
@@ -26,24 +25,28 @@ while true; do
 done
 [[ -z "$site" ]] && usage
 
-echo "Warning: This script has been deprecated and replaced by `/operations/cronjob.sh`"
-cron_key=$(drush  state-get system.cron_key --format=string)
+while [ ! -f "/var/run/cronkey" ]; do
+    sleep 10s;
+done
+
+cron_key=$(cat /var/run/cronkey)
 if echo "$cron_key" | grep -Eq '^[[:alnum:]]_-]+$' ; then # key can contain alphabetic, numeric, and two special characters.
     echo "$(date) : [${site}] Error cron key not valid. Website might be broken."
-    continue
 fi
 
-# We have max time / keep alive due to some crons to take longer than expected (it has been reported in Oct/2021 of a website[test-cnpdfs] to take ~5/6min)
-HTTP_CODE=$(curl --location --silent -L --max-time 900  --keepalive-time 900 -w "%{http_code}\n" "http://${site}/cron/${cron_key}" -o /dev/null)
-case $HTTP_CODE in
-    204)
-        echo "$(date) : [${site}] ${HTTP_CODE/204/OK}" # Logs not added since pod logs should suffice ? >> "$LOGFILE_MASTER"  204 No Content -> success
-    ;;
-    504)
-        echo "$(date) : [${site}] ${HTTP_CODE/504/NOT OK, ignoring since server might be going through heavy load...}"
-    ;;
-    *)
-        echo "$(date) : [${site}] Error code, expected 204, got ${HTTP_CODE}"
-        exit 1
-    ;;
-esac
+while true; do
+    # We have max time / keep alive due to some crons to take longer than expected (it has been reported in Oct/2021 of a website[test-cnpdfs] to take ~5/6min)
+    HTTP_CODE=$(curl --location --silent -L --max-time 900  --keepalive-time 900 -w "%{http_code}\n" "http://${site}/cron/${cron_key}" -o /dev/null)
+    case $HTTP_CODE in
+        204)
+            echo "$(date) : [${site}] ${HTTP_CODE/204/OK}" # Logs not added since pod logs should suffice ? >> "$LOGFILE_MASTER"  204 No Content -> success
+        ;;
+        504)
+            echo "$(date) : [${site}] ${HTTP_CODE/504/NOT OK, ignoring since server might be going through heavy load...}"
+        ;;
+        *)
+            echo "$(date) : [${site}] Error code, expected 204, got ${HTTP_CODE}"
+        ;;
+    esac
+    sleep 30m
+done
