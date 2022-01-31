54d9e0cb23e57c86e84174e739599a3b009a6ba3
Konstantinos Samaras-Tsakiris <konstantinos.samaras-tsakiris@cern.ch>
2022-01-31 19:45:55 +0100

    Merge branch 'probes' into 'master'
    Add script for probes
    
    See merge request drupal/paas/cern-drupal-distribution!79

--- images/php-fpm/run-php-fpm.sh
+++ images/php-fpm/run-php-fpm.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 set -ex
 
+LIVENESS_PROBE_FAILURE_FILE=/var/run/liveness_probe_failure
+
 # Export cron_key value to a file in emptyDir
 echo $(drush  state-get system.cron_key --format=string) > /var/run/cronkey &
+
+# Check if php-fpm container has been restarted due to Liveness probe
+if [ -f "${LIVENESS_PROBE_FAILURE_FILE}" ]; then
+    if [[ $(wc -l <${LIVENESS_PROBE_FAILURE_FILE}) -ge 3 ]]; then
+        rm -rf ${LIVENESS_PROBE_FAILURE_FILE}
+        echo "Multiple liveness probe failures detected. Attempt to restore by clearing caches."
+        drush cr
+    fi
+fi
 
 # Run PHP-FPM
 exec php-fpm
