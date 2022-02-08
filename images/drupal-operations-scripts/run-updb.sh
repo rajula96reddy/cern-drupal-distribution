6c958d285d0bc87b353d8917847d042255fc406b
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2022-02-08 17:05:12 +0100

    Keep updb logs

--- images/drupal-operations-scripts/run-updb.sh
+++ images/drupal-operations-scripts/run-updb.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+LOGS_DIR="/drupal-data/updb-logs/"
+DATE=$(date -u +%Y.%m.%dT%H-%M-%SZ)
+
 # Change working directory to the drupal code
 cd /app
+[[ -d "$LOGS_DIR" ]] || mkdir "$LOGS_DIR"
 
 # Run update db
 echo "Running drush updatedb"
 /operations/enable-maintenance-mode.sh
-drush updatedb -y
+drush updatedb -y 1>&2 2> "$LOGS_DIR/$DATE"
 /operations/disable-maintenance-mode.sh
