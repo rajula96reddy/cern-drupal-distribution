9001b3796fa084eeddbc37eed2133f2d65c348c6
Konstantinos Samaras-Tsakiris <konstantinos.samaras-tsakiris@cern.ch>
2022-02-17 16:38:00 +0100

    Merge branch 'probe-max-time' into 'master'
    Increase probe timeout
    
    See merge request drupal/paas/cern-drupal-distribution!91

--- images/drupal-operations-scripts/startup-probe-site.sh
+++ images/drupal-operations-scripts/startup-probe-site.sh
@@ -4,7 +4,7 @@
 # and then to php-fpm. The startup probe is on the php-fpm container.
 # Doing the request directly to the unix socket in php-fpm didn't work.
 # Related Issue: https://gitlab.cern.ch/webservices/webframeworks-planning/-/issues/616
-HTTP_CODE_BASE=$(curl --silent --fail --insecure -I localhost:8080/_site/_php-fpm-status -w '%{http_code}\n' -o /dev/null)
+HTTP_CODE_BASE=$(curl --max-time 200 --silent --fail --insecure -I localhost:8080/_site/_php-fpm-status -w '%{http_code}\n' -o /dev/null)
 if [[ "${HTTP_CODE_BASE}" -ne "200" ]]; then
     exit 1
 fi
